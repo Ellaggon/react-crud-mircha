d2150fed3a5ac4db6f089e14383e5d668a9f041c
Ellioth Aguirre <ellaggon@gmail.com>
2023-10-11 13:03:06 -0300

    Creacion de componentes, CrudApp, CrudForm, CrudTable, CrudTableRow

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CrudApp from './components/CrudApp'
+
+const App = () => {
+
+  return (
+    <>
+      <h2>CRUD APP</h2>
+      <CrudApp />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CrudApp.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react"
+import CrudForm from "./CrudForm"
+import CrudTable from "./CrudTable"
+
+const initialDb = [
+  {
+    id: 1,
+    name: "Seiya",
+    constellation: "Pegaso",
+  },
+  {
+    id: 2,
+    name: "Shiryu",
+    constellation: "Dragon",
+  },
+  {
+    id: 3,
+    name: "Hyoga",
+    constellation: "Cisne",
+  },
+  {
+    id: 4,
+    name: "Shun",
+    constellation: "Andromeda",
+  },
+  {
+    id: 5,
+    name: "Ikki",
+    constellation: "Fenix",
+  },
+];
+
+const CrudApp = () => {
+  const [db, setDb] = useState(initialDb);
+  const [dataToEdit, setDataToEdit] = useState(null);
+  
+  const createData = (data) => {
+    // console.log(data);
+    data.id = Date.now();
+    setDb([...db, data])
+  };
+  
+  const updateData = (data) => {};
+  
+  const deleteData = (id) => {};
+
+  return (
+    <>
+    <CrudForm 
+      createData={createData}
+      updateData={updateData}
+      dataToEdit={dataToEdit}
+      setDataToEdit={setDataToEdit}/>
+    <CrudTable 
+      data={db}
+      setDataToEdit={setDataToEdit}
+      deleteData={deleteData}/>
+    </>
+  )
+}
+
+export default CrudApp
