7d87b80fee660e454dd4f6d5610d0ea40c58379b
Ellioth Aguirre <ellaggon@gmail.com>
2023-10-21 06:09:39 -0300

    Selects Anidados: Definición de componentes y lógica

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import CrudApp from './components/CrudApp'
 import CrudApi from './components/CrudApi';
+import SelectAnidados from './components/SelectAnidados';
 
 const App = () => {
 
   return (
     <>
+      <SelectAnidados />
+      <hr />
       <CrudApi />
+      <hr />
       <CrudApp />
       <hr />
     </>
